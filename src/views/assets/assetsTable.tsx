14391d0b23810507da106902cc3b34d70d4d56a5
郭维 <guodawei1021@gmail.com>
2025-10-13 12:06:46 +0800

    Feat: Assets page uses data from websocket

--- src/views/assets/assetsTable.tsx
+++ src/views/assets/assetsTable.tsx
@@ -1,31 +1,27 @@
-import { TableHeader, TableBody } from "@/components/table-header";
-import { useTableSort, type Order } from "@/hooks/useTableHelper";
-import { type IAssetItem } from "./assetsList";
-import BuyButton from "@/components/button/BuyButton";
-import TradingHaltBtn from "@/components/button/TradingHaltBtn";
-import Pagination from "@/components/pagination";
-import { usePaginationData } from "@/hooks/useTableHelper";
-import { advancedSort } from "@/utils/sort";
-import { TextCell, TokenCell } from "./Shared"
-import { textPrefix, toFixed } from "@/utils/format";
+import { TableHeader, TableBody } from '@/components/table-header'
+import { useTableSort, type Order } from '@/hooks/useTableHelper'
+import { type IAssetItem } from './assetsList'
+import BuyButton from '@/components/button/BuyButton'
+import TradingHaltBtn from '@/components/button/TradingHaltBtn'
+import Pagination from '@/components/pagination'
+import { usePaginationData } from '@/hooks/useTableHelper'
+import { advancedSort } from '@/utils/sort'
+import { TextCell, TokenCell } from './Shared'
+import { textPrefix, toFixed } from '@/utils/format'
 
-type SortableField = "value";
-function AssetsTable(props: {
-  chainId: number;
-  account: string;
-  assetList: IAssetItem[];
-}) {
-  const { assetList } = props;
-  const { sort, onSortChange } = useTableSort<SortableField>();
+type SortableField = 'value'
+function AssetsTable(props: { chainId: number; account: string; assetsList: IAssetItem[] }) {
+  const { assetsList } = props
+  const { sort, onSortChange } = useTableSort<SortableField>()
 
   const { paginatedData, currentPage, totalPage, onPrevClick, onNextClick } =
-    usePaginationData<IAssetItem>(assetTableConfig, assetList, sort);
+    usePaginationData<IAssetItem>(assetTableConfig, assetsList, sort)
 
   return (
     <>
       <TableHeader<SortableField, IAssetItem, unknown>
-        className="bg-[rgba(255,255,255,0.04)] rounded-lg border-none px-5"
-        lngPrefix="assets.assetsTab"
+        className='bg-[rgba(255,255,255,0.04)] rounded-lg border-none px-5'
+        lngPrefix='assets.assetsTab'
         config={assetTableConfig}
         sort={sort}
         onSortChange={onSortChange}
@@ -34,7 +30,7 @@ function AssetsTable(props: {
         data={paginatedData}
         config={assetTableConfig}
         extra={{} as unknown}
-        getKey={(item: IAssetItem) => item.token}
+        getKey={(item: IAssetItem) => item.symbol}
       />
       {totalPage > 1 && (
         <Pagination
@@ -45,57 +41,53 @@ function AssetsTable(props: {
         />
       )}
     </>
-  );
+  )
 }
 
 const assetTableConfig = [
   {
-    key: "token",
+    key: 'token',
     sortable: false,
     render: (item: IAssetItem) => (
-      <TokenCell icon={item.icon} token={item.token} name={item.name} />
+      <TokenCell icon={item.icon} token={item.symbol} name={item.name} />
     ),
   },
   {
-    key: "holdings",
+    key: 'holdings',
     sortable: false,
     render: (item: IAssetItem) => (
-      <TextCell text={item.holdings ? toFixed(item.holdings, 4) : "--"} />
+      <TextCell text={item.holdings ?? '--'} />
     ),
   },
   {
-    key: "price",
+    key: 'price',
     sortable: false,
     render: (item: IAssetItem) => {
-      const price = item.tokenPrice ?? item.rwaPrice;
-      return (
-        <TextCell text={price ? textPrefix(toFixed(price, 2), "$") : "--"} />
-      );
+      const price = item.tokenPrice ?? item.rwaPrice
+      return <TextCell text={price ? textPrefix(toFixed(price, 2), '$') : '--'} />
     },
   },
   {
-    key: "value",
+    key: 'value',
     sortable: true,
     render: (item: IAssetItem) => (
-      <TextCell
-        text={item.value ? textPrefix(toFixed(item.value, 2), "$") : "--"}
-      />
+      <TextCell text={item.value ? textPrefix(toFixed(item.value, 2), '$') : '--'} />
     ),
     sorter: (a: IAssetItem, b: IAssetItem) => (order: Order) =>
-      advancedSort(a.value ?? "0", b.value ?? "0", order),
+      advancedSort(a.value ?? '0', b.value ?? '0', order),
   },
   {
-    key: "actions",
+    key: 'actions',
     sortable: false,
     render: (item: IAssetItem) =>
       item.rwaState === undefined ? (
-        "--"
+        '--'
       ) : item.rwaState === 0 ? (
-        <BuyButton to={"/markets/trading"} />
+        <BuyButton to={'/markets/trading'} />
       ) : (
         <TradingHaltBtn />
       ),
   },
-];
+]
 
-export default AssetsTable;
+export default AssetsTable
