8b4b546668f1071bb62955f398afdee5fee6eafb
郭维 <guodawei1021@gmail.com>
2025-10-14 11:18:27 +0800

    feat: WebSocketService

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, useRoutes } from "react-router-dom";
-import BigNumber from "bignumber.js";
-import routes from "./routes";
-import { ErrorBoundary } from "./components/ErrorBoundary";
-import { Suspense, useEffect, useRef } from "react";
-import storage from "./utils/storage";
-import { useTranslation } from "./hooks/useTranslation";
+import { BrowserRouter, useRoutes } from 'react-router-dom'
+import BigNumber from 'bignumber.js'
+import routes from './routes'
+import { ErrorBoundary } from './components/ErrorBoundary'
+import { Suspense, useEffect, useRef } from 'react'
+import storage from './utils/storage'
+import { useTranslation } from './hooks/useTranslation'
 
 import { Toaster } from "./components/ui/sonner";
 import { useBaseStore } from "./stores/baseStore";
@@ -18,19 +18,19 @@ BigNumber.config({
   DECIMAL_PLACES: 80, // 足够精度，避免 DeFi 里丢失小数
   ROUNDING_MODE: BigNumber.ROUND_DOWN, // 通常用向下取整，避免超额
   EXPONENTIAL_AT: 1e9, // 禁止科学计数法
-});
+})
 
 function RoutesWrapper() {
-  return useRoutes(routes);
+  return useRoutes(routes)
 }
 
 function App() {
-  const { t, i18n } = useTranslation();
-  const { account, chainId } = useActiveWeb3();
-  const initBaseStore = useBaseStore((state) => state.init);
+  const { t, i18n } = useTranslation()
+  const { account, chainId } = useActiveWeb3()
+  const initBaseStore = useBaseStore(state => state.init)
   const setTokenWithPriceByWebSocketData = useBaseStore(
-    (state) => state.setTokenWithPriceByWebSocketData
-  );
+    state => state.setTokenWithPriceByWebSocketData
+  )
   const setStockWithPriceByWebSocketData = useBaseStore(
     (state) => state.setStockWithPriceByWebSocketData
   );
@@ -39,56 +39,59 @@ function App() {
   const framePending = useRef<Boolean>(false)
 
   useEffect(() => {
-    const lng = storage.getItem("CA_LANGUAGE") || "en";
-    i18n.changeLanguage(lng);
-  }, [i18n]);
+    const lng = storage.getItem('CA_LANGUAGE') || 'en'
+    i18n.changeLanguage(lng)
+  }, [i18n])
 
   // 两个汇总到一起处理了
   // 获取余额信息
-  useTokenBalances();
+  useTokenBalances()
   // 获取Rwa余额
   // useRwaBalances()
 
   useEffect(() => {
-    if (!chainId) return;
+    if (!chainId) return
     // 初始化baseStore
-    initBaseStore(chainId);
-  }, [chainId]);
+    initBaseStore(chainId)
+  }, [chainId])
 
   useEffect(() => {
-    wsService.init({});
-    wsService.subscribe(["summary"], (data) => {
+    wsService.init({})
+
+    const listener = (data: any) => {
       if (!framePending.current) {
         framePending.current = true
         requestAnimationFrame(() => {
-          if (data.type === "summary") {
+          if (data.type === 'summary') {
             setTokenWithPriceByWebSocketData(data.data || [])
             setStockWithPriceByWebSocketData(data.data || [])
             stableTokenWithPrice(data.data || [])
           }
           framePending.current = false
         })
-        
       }
-      
-    });
+    }
+
+    wsService.on('summary', listener)
 
     return () => {
-      wsService.close();
-    };
-  }, []);
+      wsService.off('summary', listener)
+
+      wsService.close()
+    }
+  }, [])
 
   return (
-    <ErrorBoundary fallback={<h2>{t("pageError")}</h2>}>
-      <Suspense fallback={<div>{t("Loading")}...</div>}>
+    <ErrorBoundary fallback={<h2>{t('pageError')}</h2>}>
+      <Suspense fallback={<div>{t('Loading')}...</div>}>
         <BrowserRouter>
           <ScrollToTop />
           <RoutesWrapper />
         </BrowserRouter>
-        <Toaster position="top-center" />
+        <Toaster position='top-center' />
       </Suspense>
     </ErrorBoundary>
-  );
+  )
 }
 
-export default App;
+export default App
