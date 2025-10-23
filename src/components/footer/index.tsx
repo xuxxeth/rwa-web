a47a43f4bf26ef2610c3a06650c641319d0f15e2
郭维 <guodawei1021@gmail.com>
2025-10-23 09:52:44 +0800

    feat: add two kinds of policy pdf file

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,51 +1,64 @@
-import { TG_URL, X_URL } from "@/config/constants";
-import { useTranslation } from "@/hooks/useTranslation";
-import { MainLayout } from "@/layouts/main";
+import { TG_URL, X_URL } from '@/config/constants'
+import { useTranslation } from '@/hooks/useTranslation'
+import { MainLayout } from '@/layouts/main'
+import { openUrlInNewWindow } from '@/utils/index'
 
 export function XFooter() {
   const { t } = useTranslation()
   return (
     <MainLayout>
-      <div className="flex justify-between items-center border-t border-[rgba(255,255,255,0.1)] pt-[46px] mt-[130px] pb-[130px] px-5 font-normal">
+      <div className='flex justify-between items-center border-t border-[rgba(255,255,255,0.1)] pt-[46px] mt-[130px] pb-[130px] px-5 font-normal'>
         <div>
-          <img src="/images/logo_text.png" className="w-[182px]" alt="" />
-          <div className=" text-base text-80 mt-[76px]">@ 2025 Cyber Alpha. {t('footer.text1')}</div>
+          <img src='/images/logo_text.png' className='w-[182px]' alt='' />
+          <div className=' text-base text-80 mt-[76px]'>
+            @ 2025 Cyber Alpha. {t('footer.text1')}
+          </div>
         </div>
-        <div className="flex text-base text-white gap-x-[100px]">
+        <div className='flex text-base text-white gap-x-[100px]'>
           <div>
-            <div className=" font-semibold">{t('footer.text5')}</div>
-            <a href={X_URL} target="_blank">
-              <div className="flex items-center mt-6">
-                <img src="/images/icons/x.png" className="w-6" alt="" />
-                <div className="text-80 ml-2">X Official</div>
+            <div className=' font-semibold'>{t('footer.text5')}</div>
+            <a href={X_URL} target='_blank'>
+              <div className='flex items-center mt-6'>
+                <img src='/images/icons/x.png' className='w-6' alt='' />
+                <div className='text-80 ml-2'>X Official</div>
               </div>
             </a>
-            <a href={TG_URL} target="_blank">
-              <div className="flex items-center mt-8">
-                <img src="/images/icons/tg.png" className="w-6" alt="" />
-                <div className="text-80 ml-2">Telegram</div>
+            <a href={TG_URL} target='_blank'>
+              <div className='flex items-center mt-8'>
+                <img src='/images/icons/tg.png' className='w-6' alt='' />
+                <div className='text-80 ml-2'>Telegram</div>
               </div>
             </a>
           </div>
           <div>
-            <div className=" font-semibold">{t('About')}</div>
-            <div className="flex items-center mt-6">
-              <div className="text-80">{t('FAQ')}</div>
+            <div className=' font-semibold'>{t('About')}</div>
+            <div className='flex items-center mt-6'>
+              <div className='text-80'>{t('FAQ')}</div>
             </div>
-            <div className="flex items-center mt-8">
-              <div className="text-80">{t('footer.text2')}</div>
+            <div className='flex items-center mt-8'>
+              <div
+                className='text-80 cursor-pointer'
+                onClick={() => openUrlInNewWindow('/documents/cyberalpha-privacy-policy.pdf')}
+              >
+                {t('footer.text2')}
+              </div>
             </div>
-            <div className="flex items-center mt-8">
-              <div className="text-80">{t('footer.text3')}</div>
+            <div className='flex items-center mt-8'>
+              <div
+                className='text-80 cursor-pointer'
+                onClick={() => openUrlInNewWindow('/documents/cyberalpha-terms-of-service.pdf')}
+              >
+                {t('footer.text3')}
+              </div>
             </div>
           </div>
           <div>
-            <div className=" font-semibold">{t('footer.text4')}</div>
-            <a href="mailto:contact@cyberalpha.cc">
-            <div className="flex items-center mt-6">
-              <img src="/images/icons/e_mail.png" className="w-6" alt="" />
-              <div className="text-80 ml-2">contact@cyberalpha.cc</div>
-            </div>
+            <div className=' font-semibold'>{t('footer.text4')}</div>
+            <a href='mailto:contact@cyberalpha.cc'>
+              <div className='flex items-center mt-6'>
+                <img src='/images/icons/e_mail.png' className='w-6' alt='' />
+                <div className='text-80 ml-2'>contact@cyberalpha.cc</div>
+              </div>
             </a>
           </div>
         </div>
