353507dc770ddaba5b5732ef7fa93f1302e36338
Ton <yjxx0607@gmail.com>
2025-12-24 18:51:45 +0800

    fix: Kyc缓存信息，全局清理 (#258)

--- src/components/Updater.tsx
+++ src/components/Updater.tsx
@@ -1,21 +1,24 @@
+import { useActiveWeb3 } from "@/hooks/useActiveWe3";
 import { useNetworkStatus } from "@/hooks/useNetworkStatus";
 import { useWssOn } from "@/hooks/useWssOn";
 import { useBaseStore } from "@/stores/baseStore";
 import { useWssStore } from "@/stores/wssStore";
-import { lazy, memo, useEffect } from "react";
+import storage from "@/utils/storage";
+import { KYC_UPLOAD_STORAGE_KEY } from "@/views/identity/components/Upload/shared";
+import { lazy, memo, useEffect, useRef } from "react";
 const KycState = lazy(() => import("@/components/kyc-state"));
 const Compliance = lazy(() => import("@/components/compliance"));
 
 const Updater = memo(
   () => {
-    const { 
-      isOnline, 
-      blockNumber, 
-      error 
-    } = useNetworkStatus({
-      interval: 15000 // 15秒检查一次
-    });
-
+    // const { 
+    //   isOnline, 
+    //   blockNumber, 
+    //   error 
+    // } = useNetworkStatus({
+    //   interval: 15000 // 15秒检查一次
+    // });
+    const { account } = useActiveWeb3()
     const newOrder = useWssStore(state => state.newOrder)
     const freshTokenBalances = useBaseStore(state => state.freshTokenBalances)
     useEffect(() => {
@@ -39,6 +42,16 @@ const Updater = memo(
       setStockWithPriceByWebSocketData(data || [])
       stableTokenWithPrice(data || [])
     })
+
+
+    const preAccount = useRef<string | undefined>(undefined)
+    useEffect(() => {
+      if (account && preAccount.current && account !== preAccount.current) {
+        storage.removeItem(KYC_UPLOAD_STORAGE_KEY)
+        storage.removeItem('kycBaseInfo')
+      }
+      preAccount.current = account
+    }, [account])
 
     return (
       <>
