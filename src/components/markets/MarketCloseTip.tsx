66b357e12dd833c599d84c38fe0bbcbc1e41979a
Ton <yjxx0607@gmail.com>
2026-03-27 09:47:02 +0800

    fix: 交易提示 (#491)

--- src/components/markets/MarketCloseTip.tsx
+++ src/components/markets/MarketCloseTip.tsx
@@ -9,19 +9,35 @@ const MarketCloseTip = () => {
   const { t } = useTranslation()
   const marketTradeState = useBaseStore(state => state.marketTradeState)
   const tradeType = useTradeStore(state => state.tradeType)
-  if (marketTradeState === MARKET_STATUS.OPEN || tradeType === TradeType.LIMIT) return null
-  return (
-    <div className="p-3 bg-[#1A1B1E] rounded-[4px] text-[12px] text-[#FFB219] flex mt-3">
-      <div className="w-[18px] h-[18px] shrink-0 mr-2">
-        <LazyImage src="/images/v2/icons/warning.png" className="w-[18px] h-[18px]" />
+  if (tradeType === TradeType.MARKET && (marketTradeState !== MARKET_STATUS.OPEN)) {
+    return (
+      <div className="p-3 bg-[#1A1B1E] rounded-[4px] text-[12px] text-[#FFB219] flex mt-3">
+        <div className="w-[18px] h-[18px] shrink-0 mr-2">
+          <LazyImage src="/images/v2/icons/warning.png" className="w-[18px] h-[18px]" />
+        </div>
+        
+        <div>
+          {t('v2.tx.t23')}
+        </div>
+        
       </div>
-      
-      <div>
-        {t('v2.tx.t23')}
+    )
+  } 
+  if (tradeType === TradeType.LIMIT && marketTradeState === MARKET_STATUS.CLOSE) {
+    return (
+      <div className="p-3 bg-[#1A1B1E] rounded-[4px] text-[12px] text-[#FFB219] flex mt-3">
+        <div className="w-[18px] h-[18px] shrink-0 mr-2">
+          <LazyImage src="/images/v2/icons/warning.png" className="w-[18px] h-[18px]" />
+        </div>
+        
+        <div>
+          {t('v2.tx.t231')}
+        </div>
+        
       </div>
-      
-    </div>
-  )
+    )
+  }
+  return null
 }
 
 export { MarketCloseTip }
