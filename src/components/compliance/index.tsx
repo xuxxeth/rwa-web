dc652c1e026e6fc6d445bd91263b3bd67822af8a
Guowei <138768192+time-endless@users.noreply.github.com>
2026-04-29 14:28:06 +0800

    fix: 修复合约页面账号切换问题 (#575)
    Co-authored-by: guowei <guoweihd@126.com>

--- src/components/compliance/index.tsx
+++ src/components/compliance/index.tsx
@@ -22,9 +22,9 @@ const Compliance = () => {
   const [loading, setLoading] = useState(false)
 
   const getAgreementsAccepted = async () => {
-    setLoading(true)
+    // setLoading(true)
     const res = await kycApi.getAgreementsAccepted()
-    setLoading(false)
+    // setLoading(false)
     if (res && res.data && !res.data.privacy) {
       setShow(true)
       lock()
@@ -57,6 +57,8 @@ const Compliance = () => {
   useEffect(() => {
     if (account && isSignatureValid) {
       getAgreementsAccepted()
+    } else {
+      setShow(false)
     }
   }, [account, isSignatureValid])
 
