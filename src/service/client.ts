fe50940acc6849dd2a9bf2d456f52a52c389a61a
Ton <yjxx0607@gmail.com>
2026-07-02 10:51:37 +0800

    feat: 不支持的链可签署协议 (#655)

--- src/service/client.ts
+++ src/service/client.ts
@@ -74,6 +74,7 @@ const axiosInstance: AxiosInstance = axios.create({
 
 const AUTH_URL_PREFIX = ['/scan/api/', '/kyc/api/', '/uc/api', '/risk/api/', '/ref/api/'] // 需要授权的接口前缀列表
 const NO_CHAIN_ID_HEADER_URL_SUFFIX = ['/base/public/chains']
+const NO_SUPPORTED_CHAIN_URL_SUFFIX = ['/v1/uc/api/agreements/accept'] // 不支持的链的接口后缀列表
 
 function handleReqSignature(req: InternalAxiosRequestConfig, controller: AbortController, account: string) {
   const url = req.url || ''
@@ -123,7 +124,8 @@ axiosInstance.interceptors.request.use((req: InternalAxiosRequestConfig) => {
   const { account, chainId, isChainSupported } = getConnectStateFromStorage()
 
   // 拦截住不支持的 chain 的请求
-  if (chainId && !isChainSupported) {
+  // 签署协议，不区分链，不需要拦截
+  if (chainId && !isChainSupported && !NO_SUPPORTED_CHAIN_URL_SUFFIX.some(suffix => url.includes(suffix))) {
     controller.abort()
     return Promise.reject(new axios.Cancel(`Chain ID ${chainId} is not supported`))
   }
